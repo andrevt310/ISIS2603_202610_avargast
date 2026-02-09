b3485b8f7199e83396450d7965a03821f01a5e03
Andrea Vargas <a.vargast@uniandes.edu.co>
2026-02-08 20:53:59 -0500

    Cambios actor

--- src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/ActorEntity.java
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/ActorEntity.java
@@ -3,10 +3,14 @@ package co.edu.uniandes.dse.TallerPersistencia.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import lombok.Data;
 
-public class ActorEntity {
+@Data
+@Entity
+public class ActorEntity extends BaseEntity {
+
     String nombre;
     String nacionalidad;
 
